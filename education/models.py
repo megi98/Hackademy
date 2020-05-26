244dbaf1175fac999e8b5963d6464b4faf9fee76
megi98 <megi__1998@abv.bg>
2020-05-26 17:34:46 +0300

    update models.py

--- education/models.py
+++ education/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Courses(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=250, unique=True)
     description = models.TextField()
-    start_date = models.DateTimeField()
+    start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f'Course {self.name}'
 
     @property
     def duration(self):
-        return (self.end_date - self.start_date).days // 7
-
-    def __str__(self):
-        return self.name
+        if self.end_date:
+            return self.end_date - self.start_date
 
 
 class Lectures(models.Model):
