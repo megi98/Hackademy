94756efd146dd004d824f07e2e7196c0cd39b5ad
megi98 <megi__1998@abv.bg>
2020-05-27 14:24:48 +0300

    add all admin pages

--- education/admin.py
+++ education/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from education.models import Courses
+from education.models import (Courses, Lectures, Tasks, Solutions)
 
 
 @admin.register(Courses)
@@ -14,3 +13,23 @@ class CourseAdmin(admin.ModelAdmin):
         return 'N/A'
 
     get_duration.short_description = 'Duration'
+
+
+@admin.register(Lectures)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'week', 'course', 'url')
+
+
+@admin.register(Tasks)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'due_date', 'course', 'lecture')
+
+
+@admin.register(Solutions)
+class SolutionAdmin(admin.ModelAdmin):
+    list_display = ('task', 'date', 'url')
+
+    def get_task_description(self, obj):
+        return obj.task.description
+
+    get_task_description.short_description = 'description'
